cafbba2a12ab1e6f5bb6de38d92fa685eed83dae
pborole <borole.pankaj@gmail.com>
2021-04-17 18:17:40 +0530

    first
    ok

---
+++ ShopBridgeApi/Controllers/InventoriesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ShopBridgeApi.Models;
+
+namespace ShopBridgeApi.Controllers
+{
+    public class InventoriesController : ApiController
+    {
+        private ShopBridgeEntities db = new ShopBridgeEntities();
+
+        // GET: api/Inventories
+        public IQueryable<Inventory> GetInventories()
+        {
+            return db.Inventories;
+        }
+
+        // GET: api/Inventories/5
+        [ResponseType(typeof(Inventory))]
+        public IHttpActionResult GetInventory(int id)
+        {
+            Inventory inventory = db.Inventories.Find(id);
+            if (inventory == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(inventory);
+        }
+
+        // PUT: api/Inventories/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutInventory(int id, Inventory inventory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != inventory.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(inventory).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!InventoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Inventories
+        [ResponseType(typeof(Inventory))]
+        public IHttpActionResult PostInventory(Inventory inventory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Inventories.Add(inventory);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = inventory.ID }, inventory);
+        }
+
+        // DELETE: api/Inventories/5
+        [ResponseType(typeof(Inventory))]
+        public IHttpActionResult DeleteInventory(int id)
+        {
+            Inventory inventory = db.Inventories.Find(id);
+            if (inventory == null)
+            {
+                return NotFound();
+            }
+
+            db.Inventories.Remove(inventory);
+            db.SaveChanges();
+
+            return Ok(inventory);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool InventoryExists(int id)
+        {
+            return db.Inventories.Count(e => e.ID == id) > 0;
+        }
+    }
+}
